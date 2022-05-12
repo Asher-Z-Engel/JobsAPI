9ce49e2c2d7696f043f532606325f10e5b489f9c
Asher <asherzengel@gmail.com>
2022-05-12 21:26:49 +0300

    User model, login and register functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,27 @@
+const User = require('../models/User')
+const { BadRequestError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
+const bcrypt = require('bcryptjs')
 
 const login = async (req, res) => {
-  res.status(StatusCodes.OK).json({success: true, msg: 'Logged in'})
+  const { email, password } = req.body
+  if (!email || !password) {
+    throw new BadRequestError('Email and password are required')
+  }
+  const user = await User.findOne({ email })
+  if (!user) {
+    throw new BadRequestError('Invalid Credentials')
+  }
+  const isPasswordCorrect = await user.checkPassword(password)
+  if (!isPasswordCorrect) {
+    throw new BadRequestError('Invalid Credentials')
+  }
+  res.status(StatusCodes.ACCEPTED).json({success: true, name: user.name, token: user.createToken()})
 }
 
 const register = async (req, res) => {
-  res.status(StatusCodes.ACCEPTED).json({success: true, msg: 'Registered user'})
+  const user = await User.create(req.body)
+  res.status(StatusCodes.CREATED).json({success: true, name: user.name, token: user.createToken()})
 }
 
 module.exports = {
