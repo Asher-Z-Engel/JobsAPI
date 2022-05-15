4a2b8ad2997be0c348bd3ea125fb64a490a92f54
Asher <asherzengel@gmail.com>
2022-05-15 17:36:07 +0300

    authentication middleware

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,10 +1,22 @@
 const { AuthError } = require('../errors')
 const jwt = require('jsonwebtoken')
+const User = require('../models/User')
 
 const authenticate = async (req, res, next) => {
   const authHeader = req.headers.authorization
-  console.log(authHeader)
-  next()
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    throw new AuthError('You are not authorized to access this resource')
+  }
+  const token = authHeader.split(' ')[1]
+  const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
+  const { userId, name } = decodedToken
+  try {
+    await User.findById(userId)
+    req.user = {userId, name}
+    next()
+  } catch (error) {
+    throw new AuthError('You are not authorized to access this resource')
+  }
 }
 
 module.exports = authenticate
